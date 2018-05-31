7af6fb096ce022c038eff0381616dc16699a01dc
SirNexus <aidan.carson@nginx.com>
2018-05-31 16:50:17 -0700

    Add login, better logo

---
+++ connectvars.php
@@ -0,0 +1,9 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_steinehz');
+  define('DB_PASSWORD', '7019');
+  define('DB_NAME', 'cs340_steinehz');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_steinehz');
+?>
+
