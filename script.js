038c9c687748c8f400d372978012e7db67659b47
SirNexus <aidan.carson@nginx.com>
2018-06-05 19:22:11 -0700

    Add Spotify API Hooki In

--- script.js
+++ script.js
@@ -45,6 +45,67 @@ $(document).ready(function(){
     });
 });
 
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = 'BQAHdQtOc-YvNTrT8YvyXuRjt_JjZBKhhtu0MDxlktb_Jhu4GVMW68-tcEgGR3wAuu7yhI9TyGxYwGbVRENPAKEMV1_G84SX9OdcQUa6e64dAN9KDhfOHK3bV2xbs_EvirBKq09cZBLHcbKNzzt_8u7MASK0MtW4Ibe4lA';
+    const player = new Spotify.Player({
+      name: 'Float On',
+      getOAuthToken: cb => { cb(token); }
+    });
+  
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+  
+    // Playback status updates
+    player.addListener('player_state_changed', state => { console.log(state); });
+  
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+    });
+  
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+  
+    // Connect to the player!
+    player.connect();
+
+    // $.ajax({
+    //     url: 'https://api.spotify.com/v1/tracks/2lwwrWVKdf3LR9lbbhnr6R',
+    //     headers: {
+    //         'Authorization': 'Bearer ' + token
+    //     },
+    //     success: function(response) {
+    //         console.dir(response);
+    //     }
+    // });
+
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me/player/play',
+        type: 'PUT',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        data: JSON.stringify({"uris": ["spotify:track:2lwwrWVKdf3LR9lbbhnr6R"]}),
+        contentType: "application/json",
+        dataType: "json",
+        processData: false,
+        success: function(response) {
+            console.log("Success:" + response)
+        },
+        error: function(error) {
+            console.log("Error:")
+            console.dir(error)
+        }
+    });
+      
+  };
+  
+
 function insertSong() {
 	console.log("here");
     var div = document.getElementById("dom-target");
