2dbeca7cff0634d2090571fec7ade56d2c07b71f
Daniel Grocki <grockid@flip3.engr.oregonstate.edu>
2018-06-05 19:51:39 -0700

    fixed css for the artists

---
+++ playlists.php
@@ -0,0 +1,83 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+        <meta charset="UTF-8"> 
+    </head>
+    <body>
+        <div class="header">
+            <img class="header-logo" src="yfitops_header.jpg">
+            <div class="container header-text text-center">Profile</div>
+            <button type="button" id="header-profile" class="m-4 btn btn-primary header-nav">Profile</button>
+            <button type="button" id="header-logout" class="my-4 mx-1 btn btn-primary header-nav">Log Out</button>
+        </div>
+
+        <div class="left-header">
+            <button type="button" id="discover-btn" class="m-4 btn btn-primary header-btn">Discover</button>
+            <button type="button" id="playlists-btn" class="m-4 btn btn-primary header-btn">Playlists</button>
+            <button type="button" id="songs-btn" class="m-4 btn btn-primary header-btn">Songs</button>
+            <button type="button" id="friends-btn" class="m-4 btn btn-primary header-btn">Friends</button>
+        </div>
+
+        <div class="my-3 container profile-text text-center">
+            Your Playlists
+        </div>
+
+        <div id="playlist-container" class="d-flex container main-container">
+        <!--    <div class="m-4 playlist box">Text box</div>
+            <div class="m-4 playlist box">Text box</div>
+            <div class="m-4 playlist box">Text box</div>
+            <div class="m-4 playlist box">Text box</div>
+            <div class="m-4 playlist box">Text box</div>
+            <div class="m-4 playlist box">Text box</div>
+            <div class="m-4 playlist box">Text box</div> -->
+		</div>
+
+<div id="dom-target"  style="display: none;" > 
+<?php
+	include 'connectvars.php'; 
+
+
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+		
+	$query = "SELECT P.Title FROM Playlist P WHERE P.Username = 'grockidile'";
+	
+	
+
+	$result = mysqli_query($conn, $query);
+	
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+	
+// get number of columns in table	
+	$fields_num = mysqli_num_fields($result);
+	
+	while($row = mysqli_fetch_row($result)) {
+		$a = array();	
+		foreach($row as $cell)		
+			array_push($a, $cell);
+
+		$cs = implode("," , $a);
+		echo htmlspecialchars($cs); 
+		echo "<script>insertPlaylist()</script>";
+		
+
+//		echo "<script> insertSong(json_encode($a[0]), 'a thing','another thing' , 'more things')</script>";
+	}
+
+	mysqli_free_result($result);
+	mysqli_close($conn);
+?>
+
+
+</div>
+
+    </body>
+</html>
--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     });
 
     $("#playlists-btn").click(function(){
-        window.location.href = "./playlists.html";
+        window.location.href = "./playlists.php";
     });
 
     $("#songs-btn").click(function(){
@@ -29,7 +29,7 @@ $(document).ready(function(){
     });
     
     $("#friends-btn").click(function(){
-        window.location.href = "./friends.html";
+        window.location.href = "./friends.php";
     });
 
     $("#discover-btn").click(function(){
@@ -37,7 +37,7 @@ $(document).ready(function(){
     });
 
     $("#header-profile").click(function(){
-        window.location.href = "./profile.html";
+        window.location.href = "./profile.php";
     });
 
     $("#header-logout").click(function(){
@@ -45,12 +45,106 @@ $(document).ready(function(){
     });
 });
 
+
+function insertArtistProfile() {
+    var div = document.getElementById("dom-target");
+	var myData = div.textContent;
+
+	var array = myData.split(",");
+	console.log(array);
+
+	var artist_name = array[0];
+	var image_url = array[1].substring(0,array[1].indexOf("insert"));
+
+	var elem = document.getElementById('artists-container');
+//  var lastSong = $('#songs-container').children().last();
+//	console.log(lastSong);
+
+
+    var node = document.createElement('div');
+	node.classList.add("m-4");
+    node.classList.add("artistBox");
+    
+	var circle = document.createElement('div');
+	circle.classList.add("artist");
+	node.appendChild(circle);
+
+	var img = document.createElement('IMG');
+	img.setAttribute("src", image_url);
+	img.classList.add("artistImg");
+	circle.appendChild(img);
+
+	var text = document.createElement('div');
+	text.innerHTML = artist_name;
+	node.appendChild(text);
+
+	div.innerHTML = "";
+    
+	elem.append(node);
+
+
+}
+function insertPlaylist() {
+    var div = document.getElementById("dom-target");
+	var myData = div.textContent;
+
+	var array = myData.split(",");
+	console.log(array);
+
+	var playlist = array[0];
+	var playlist = array[0].substring(0,array[0].indexOf("insert"));
+
+	var elem = document.getElementById('playlist-container');
+//  var lastSong = $('#songs-container').children().last();
+//	console.log(lastSong);
+
+
+    var node = document.createElement('div');
+    node.classList.add("playlist");
+    node.classList.add("m-4");
+    node.classList.add("box");
+    node.innerHTML = playlist;
+
+	div.innerHTML = "";
+    
+	elem.append(node);
+
+}
+
+function insertFriend() {
+	console.log("here");
+    var div = document.getElementById("dom-target");
+	var myData = div.textContent;
+
+	var array = myData.split(",");
+	console.log(array);
+
+	var friend = array[0];
+	var friend = array[0].substring(0,array[0].indexOf("insert"));
+	console.log(friend);
+
+	var elem = document.getElementById('friends-container');
+//  var lastSong = $('#songs-container').children().last();
+//	console.log(lastSong);
+
+
+    var node = document.createElement('div');
+    node.classList.add("artist");
+    node.classList.add("m-4");
+    node.classList.add("box");
+    node.innerHTML = friend;
+
+	div.innerHTML = "";
+    
+	elem.append(node);
+
+}
+
+
 function insertSong() {
 	console.log("here");
     var div = document.getElementById("dom-target");
 	var myData = div.textContent;
-	
-
 
 	var array = myData.split(",");
 	console.log(array);
@@ -58,7 +152,7 @@ function insertSong() {
 	var title = array[0];
 	var album = array[1];
 	var artist = array[2];
-	var genre = array[3];
+	var genre = array[3].substring(0,array[3].indexOf("insert"));
 
 	var lastSong = document.getElementById('songs-container');
 //    var lastSong = $('#songs-container').children().last();
@@ -83,8 +177,8 @@ function insertSong() {
     node.appendChild(attribute);
 
     attribute = document.createElement('div');
-    attribute.classList.add("song-artist");
-    attribute.innerHTML = artist;
+    attribute.classList.add("song-genre");
+    attribute.innerHTML = genre;
     node.appendChild(attribute);
 
 	div.innerHTML = "";
